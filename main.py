d80f3c441959f8ff954cf2940b1ad089c24f1cee
AbdulghaniAlhazmi <Abdulghani.Hassan.jed.k2@tuwaiq.edu.sa>
2022-04-19 14:44:01 +0300

    Lab1 Solved

---
+++ main.py
@@ -0,0 +1,21 @@
+favouritePlant = "Flower"
+
+x :int = 55
+y :float = 3.14
+
+a1 = a2 = a3 = "a1 , a2 , a3 has Same Value" 
+
+
+b1,b2,b3 = 12, 13, 14
+
+
+
+print(favouritePlant)
+print(x)
+print(y)
+
+print(a1)
+print(a3)
+
+
+print(b1)
