0d383c90748bef80c77c8c7800143c2ca4256720
Areejalharbi57 <areej.mohammad.ruh.s@tuwaiq.edu.sa>
2022-04-19 14:56:28 +0300

    lab1 is added

---
+++ lab1.py
@@ -0,0 +1,17 @@
+favouritePlant = "cactus"
+x = 5
+y = 10.5
+
+print(x)
+print(y)
+
+python = java = swift = kotlin = "Programming Languages"
+print(python)
+print(java)
+print(swift)
+print(kotlin)
+
+name , course , time = "Areej", "pythone", "12:00 - 4:00"
+print(name)
+print(course)
+print(time)
