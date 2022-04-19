03d025ffd6cadbaff1844585ea32c2d53645283f
faisalha98 <fisal.alharthi.jed.k1@tuwaiq.edu.sa>
2022-04-19 21:19:23 +0300

    solve HW 1

---
+++ lap1.py
@@ -0,0 +1,21 @@
+# ____________ Q1 ____________
+
+plant = "Fan Flower"
+
+print(plant)
+# ____________ Q2 ____________
+
+x:int = 100
+y:float = 200.222
+print(x,y)
+
+# ____________ Q3 ____________
+first = second = third = 10
+print(first,second,third)
+
+# ____________ Q4 ____________
+
+a,b = 1,2
+
+print(a,b)
+
