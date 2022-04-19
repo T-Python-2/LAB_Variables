c5058d62eefe6832e46f190f4d094a61b9941ec5
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2022-04-19 14:54:47 +0300

    solved

---
+++ lab1.py
@@ -0,0 +1,16 @@
+
+#1 Define a variable that has a value of your favourite plant.
+myPlant = "Orchid"
+
+
+#2 Define “x“ variable has an integer value and variable “y” has a float value.
+x:int = 4
+y:float = 4.33
+
+#3 Assign One Value to Multiple Variables.
+val1 = val2 = val3 = "Python"
+
+#4 Assign Many Values to Multiple Variables.
+my_name  , age  = "Lama" , 23
+
+
