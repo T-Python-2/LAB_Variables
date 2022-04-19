b4847278232a2cd7618b69be890431f740f79896
esrasalam <esra.s.a@outlook.com>
2022-04-19 15:22:09 +0300

    Esra Alali First Lab is Done

---
+++ lab1.py
@@ -0,0 +1,20 @@
+
+
+# Define a variable that has a value of your favourite plant.
+my_favorite_Plant:str = "Flower"
+
+# Define “x“ variable has an integer value and variable “y” has a float value.
+x:int = 5
+y:float = 5.5
+
+# Assign One Value to Multiple Variables.
+flowers = tree = grass = "plant"
+
+#Assign Many Values to Multiple Variables.
+a, b, c = "Apple", "Banana", "Cherry"
+
+print("My favorite plant is" +" "+ my_favorite_Plant)
+print (x)
+print (y)
+print (grass)
+print ("My fav fruits are "+a,b,c)
