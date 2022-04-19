e385f04e0573ec1ebb14b5a5ce43086e43205a9e
alimatrood <alisaeedalmatrood@gmail.com>
2022-04-19 15:02:15 +0300

    Solved lab01

---
+++ lab1.py
@@ -0,0 +1,16 @@
+#LAB01 Solution
+
+#A variable to store my favorite plant
+favoritePlant:str = "Cactus"
+
+#Whole number variable(integer)
+x:int = 25 
+
+#decimal number variable(float)
+y:float = 10.56
+
+#Assign one value to multiple variables
+greetings = welcoming = hi = "Hello there!"
+
+#assign many values to multiple variables
+operatingSystem, softwareApplication, programmingLanguage = "Windows", "Visual Studio Code", "Python"
