e17be14e564e26fefd2252b3456a616c01874fc7
me <you@me.him>
2022-04-19 15:25:30 +0300

    Finished first homework

---
+++ exercise.py
@@ -0,0 +1,15 @@
+#- Define a variable that has a value of your favourite plant.
+#- Define “x“ variable has an integer value and variable “y” has a float value.
+#- Assign One Value to Multiple Variables.
+#- Assign Many Values to Multiple Variables.
+
+
+
+my_fav_plant = "Norway spruce"
+
+x , y = 2 , 2.2222
+
+firstValue,secondValue = 2
+
+firstVal , secondVal = 1,2
+
