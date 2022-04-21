d4c71c06fafe2c5d2bd928fcca3e74fe68ceab57
Eman Samkri <Eman.Jameel.jed.k2@tuwaiq.edu.sa>
2022-04-21 13:10:43 +0300

    push agin

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+Define a variable that has a value of your favourite plant.
+Define “x“ variable has an integer value and variable “y” has a float value.
+Assign One Value to Multiple Variables.
+Assign Many Values to Multiple Variables.
+'''
+
+my_plant:str = "Epipremnum"
+x : int = 12
+y : float = 12.22
+
+var1 = var2 = var3 = 12
+
+one_val = second_val = thrid_val = 12.2, 22 , 34
+
+
+print(my_plant)
+print(x)
+print(y)
+print(var1, var2 ,  var3)
+print(one_val)
